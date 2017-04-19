382b4b19f08ea31a95c109dc20a7deb93421a2a9
jspc <james.condron@beamly.com>
2017-04-19 17:41:36 +0100

    Revert "WIP: Split syntax"
    This reverts commit 955160450ee480cbe14275277af73e74c6c0e271.

--- highlight.h
+++ highlight.h
@@ -26,10 +26,13 @@ struct editorSyntax {
   int flags;
 };
 
-int editorSyntaxToColor(int hl);
-
-#include "syntax/c.h"
-
+char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
+char *C_HL_keywords[] = {
+  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "struct", "union", "typedef", "static", "enum", "class", "case",
+  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+  "void|", NULL
+};
 struct editorSyntax HLDB[] = {
   {
     "c",
@@ -39,5 +42,4 @@ struct editorSyntax HLDB[] = {
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
-
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
--- snugglebunny.c
+++ snugglebunny.c
@@ -41,7 +41,7 @@
 #include "buffer.h"
 #include "keys.h"
 #include "errors.h"
-#include "syntax.h"
+#include "highlight.h"
 #include "term.h"
 #include "snugglebunny.h"
 
@@ -236,6 +236,32 @@ void editorSelectSyntaxHighlight() {
 
       i++;
     }
+  }
+}
+
+int editorSyntaxToColor(int hl) {
+  switch (hl) {
+  case HL_NUMBER:
+    return 31;
+
+  case HL_KEYWORD2:
+    return 32;
+
+  case HL_KEYWORD1:
+    return 33;
+
+  case HL_MATCH:
+    return 34;
+
+  case HL_STRING:
+    return 35;
+
+  case HL_MLCOMMENT:
+  case HL_COMMENT:
+    return 36;
+
+  default:
+    return 37;
   }
 }
 
